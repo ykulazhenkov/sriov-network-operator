aa73b3a8dfb9e7faa5750e3f984d7766b232b434
Yury Kulazhenkov <ykulazhenkov@nvidia.com>
2024-03-01 16:05:19 +0200

    Add OVSNetwork CRD and dummy controller for it
    Signed-off-by: Yury Kulazhenkov <ykulazhenkov@nvidia.com>

--- main.go
+++ main.go
@@ -177,6 +177,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SriovNetworkPoolConfig")
 		os.Exit(1)
 	}
+	if err = (&controllers.OVSNetworkReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OVSNetwork")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
